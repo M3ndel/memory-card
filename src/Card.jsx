7baf3e46afa7a905e11f263a7c37d7cd3bedeb20
M3ndel <33386777+M3ndel@users.noreply.github.com>
2025-05-02 19:10:34 +0800

    Fix layout proportion

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,7 +2,7 @@ function Card ({ name, onClick}) {
 	const gallery = import.meta.glob('./assets/apex-char/*.jpg', { eager: true, as: 'url' })
 	const src = gallery['./assets/apex-char/' + name + '.jpg']
 	return (
-		<div style={{width: '100px', border: 'black 2px solid', background: 'gray'}} onClick={onClick}>
+		<div style={{width: '200px', border: 'black 2px solid', background: 'gray'}} onClick={onClick}>
 			<img style={{width: '100%'}} src={src} />
 			<hr />
 			<p>{name}</p>
